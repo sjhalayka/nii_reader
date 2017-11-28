663ca8a88e3250b17e8ac642f106951bbc5d1d40
Shawn Halayka <sjhalayka@gmail.com>
2017-11-28 12:19:27 -0600

    Add files via upload

--- nii.cpp
+++ nii.cpp
@@ -14,31 +14,47 @@ int main(void)
 		return 0;
 	}
 
+	size_t bytes_read = 0;
+
 	cout << "Reading header" << endl;
 
 	int sizeof_header;
 	infile.read(reinterpret_cast<char*>(&sizeof_header), sizeof(sizeof_header));
+	bytes_read += infile.gcount();
+
+	if (sizeof_header != 348)
+	{
+		cout << "Invalid header size: should be 348 bytes" << endl;
+		return -1;
+	}
 
 	char data_type[10];
 	infile.read(reinterpret_cast<char*>(&data_type), sizeof(data_type));
+	bytes_read += infile.gcount();
 
 	char db_name[18];
 	infile.read(reinterpret_cast<char*>(&db_name), sizeof(db_name));
+	bytes_read += infile.gcount();
 
 	int extents;
 	infile.read(reinterpret_cast<char*>(&extents), sizeof(extents));
+	bytes_read += infile.gcount();
 
 	short session_error;
 	infile.read(reinterpret_cast<char*>(&session_error), sizeof(session_error));
+	bytes_read += infile.gcount();
 
 	char regular;
 	infile.read(reinterpret_cast<char*>(&regular), sizeof(regular));
+	bytes_read += infile.gcount();
 
 	char dim_info;
 	infile.read(reinterpret_cast<char*>(&dim_info), sizeof(dim_info));
+	bytes_read += infile.gcount();
 
 	short dim[8];
 	infile.read(reinterpret_cast<char*>(&dim), sizeof(dim));
+	bytes_read += infile.gcount();
 
 	cout << dim[0] << " dimensions" << endl;
 	cout << "Dim 1: " << dim[1] << endl;
@@ -51,18 +67,23 @@ int main(void)
 
 	float intent_p1;
 	infile.read(reinterpret_cast<char*>(&intent_p1), sizeof(intent_p1));
+	bytes_read += infile.gcount();
 
 	float intent_p2;
 	infile.read(reinterpret_cast<char*>(&intent_p2), sizeof(intent_p2));
+	bytes_read += infile.gcount();
 
 	float intent_p3;
 	infile.read(reinterpret_cast<char*>(&intent_p3), sizeof(intent_p3));
+	bytes_read += infile.gcount();
 
 	short intent_code;
 	infile.read(reinterpret_cast<char*>(&intent_code), sizeof(intent_code));
+	bytes_read += infile.gcount();
 
 	short datatype;
 	infile.read(reinterpret_cast<char*>(&datatype), sizeof(datatype));
+	bytes_read += infile.gcount();
 
 	if (datatype != 16)
 	{
@@ -76,6 +97,7 @@ int main(void)
 
 	short bitpix;
 	infile.read(reinterpret_cast<char*>(&bitpix), sizeof(bitpix));
+	bytes_read += infile.gcount();
 
 	if (bitpix != 32)
 	{
@@ -89,102 +111,140 @@ int main(void)
 
 	short slice_start;
 	infile.read(reinterpret_cast<char*>(&slice_start), sizeof(slice_start));
+	bytes_read += infile.gcount();
 
 	float pixdim[8];
 	infile.read(reinterpret_cast<char*>(&pixdim), sizeof(pixdim));
+	bytes_read += infile.gcount();
 
 	float vox_offset;
 	infile.read(reinterpret_cast<char*>(&vox_offset), sizeof(vox_offset));
+	bytes_read += infile.gcount();
 
 	float scl_slope;
 	infile.read(reinterpret_cast<char*>(&scl_slope), sizeof(scl_slope));
+	bytes_read += infile.gcount();
 
 	float scl_inter;
 	infile.read(reinterpret_cast<char*>(&scl_inter), sizeof(scl_inter));
+	bytes_read += infile.gcount();
 
 	short slice_end;
 	infile.read(reinterpret_cast<char*>(&slice_end), sizeof(slice_end));
+	bytes_read += infile.gcount();
 
 	char slice_code;
 	infile.read(reinterpret_cast<char*>(&slice_code), sizeof(slice_code));
+	bytes_read += infile.gcount();
 
 	char xyzt_units;
 	infile.read(reinterpret_cast<char*>(&xyzt_units), sizeof(xyzt_units));
+	bytes_read += infile.gcount();
 
 	float cal_max;
 	infile.read(reinterpret_cast<char*>(&cal_max), sizeof(cal_max));
+	bytes_read += infile.gcount();
 
 	float cal_min;
 	infile.read(reinterpret_cast<char*>(&cal_min), sizeof(cal_min));
+	bytes_read += infile.gcount();
 
 	float slice_duration;
 	infile.read(reinterpret_cast<char*>(&slice_duration), sizeof(slice_duration));
+	bytes_read += infile.gcount();
 
 	float toffset;
 	infile.read(reinterpret_cast<char*>(&toffset), sizeof(toffset));
+	bytes_read += infile.gcount();
 
 	int glmax;
 	infile.read(reinterpret_cast<char*>(&glmax), sizeof(glmax));
+	bytes_read += infile.gcount();
 
 	int glmin;
 	infile.read(reinterpret_cast<char*>(&glmin), sizeof(glmin));
+	bytes_read += infile.gcount();
 
 	char descrip[80];
 	infile.read(reinterpret_cast<char*>(&descrip), sizeof(descrip));
+	bytes_read += infile.gcount();
 
 	char aux_file[24];
 	infile.read(reinterpret_cast<char*>(&aux_file), sizeof(aux_file));
+	bytes_read += infile.gcount();
 
 	short qform_code;
 	infile.read(reinterpret_cast<char*>(&qform_code), sizeof(qform_code));
+	bytes_read += infile.gcount();
 
 	short sform_code;
 	infile.read(reinterpret_cast<char*>(&sform_code), sizeof(sform_code));
+	bytes_read += infile.gcount();
 
 	float quatern_b;
 	infile.read(reinterpret_cast<char*>(&quatern_b), sizeof(quatern_b));
+	bytes_read += infile.gcount();
 
 	float quatern_c;
 	infile.read(reinterpret_cast<char*>(&quatern_c), sizeof(quatern_c));
+	bytes_read += infile.gcount();
 
 	float quatern_d;
 	infile.read(reinterpret_cast<char*>(&quatern_d), sizeof(quatern_d));
+	bytes_read += infile.gcount();
 
 	float qoffset_x;
 	infile.read(reinterpret_cast<char*>(&qoffset_x), sizeof(qoffset_x));
+	bytes_read += infile.gcount();
 
 	float qoffset_y;
 	infile.read(reinterpret_cast<char*>(&qoffset_y), sizeof(qoffset_y));
+	bytes_read += infile.gcount();
 
 	float qoffset_z;
 	infile.read(reinterpret_cast<char*>(&qoffset_z), sizeof(qoffset_z));
+	bytes_read += infile.gcount();
 
 	float srow_x[4];
 	infile.read(reinterpret_cast<char*>(&srow_x), sizeof(srow_x));
+	bytes_read += infile.gcount();
 
 	float srow_y[4];
 	infile.read(reinterpret_cast<char*>(&srow_y), sizeof(srow_y));
+	bytes_read += infile.gcount();
 
 	float srow_z[4];
 	infile.read(reinterpret_cast<char*>(&srow_z), sizeof(srow_z));
+	bytes_read += infile.gcount();
 
 	char intent_name[16];
 	infile.read(reinterpret_cast<char*>(&intent_name), sizeof(intent_name));
+	bytes_read += infile.gcount();
 
 	char magic[4];
 	infile.read(reinterpret_cast<char*>(&magic), sizeof(magic));
+	bytes_read += infile.gcount();
+
+	cout << "Read " << bytes_read << " bytes" << endl;
+
+	if (bytes_read != 348)
+	{
+		cout << "Error reading header" << endl;
+		return -1;
+	}
 
 	long unsigned int num_voxels = dim[1] * dim[2] * dim[3] * dim[4] * dim[5] * dim[6] * dim[7];
 	cout << "Reading " << num_voxels << " voxels" << endl;
 
 	vector<float> data(num_voxels, 0);
 	infile.read(reinterpret_cast<char*>(&data[0]), sizeof(float)*num_voxels);
+	bytes_read = infile.gcount();
 
-
-
-
-
-
+	if (bytes_read != sizeof(float)*num_voxels)
+	{
+		cout << "Error reading file" << endl;
+		return -1;
+	}
 
 
 	return 0;
